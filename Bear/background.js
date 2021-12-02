86825c00994014b6eada1e355ae603b7d409c683
Calvin-Jefen <80683237+Calvin-Jefen@users.noreply.github.com>
2021-12-02 19:53:08 +0700

    Updated the code
    added some new functionality that bring the program closer to the goals

--- Bear/background.js
+++ Bear/background.js
@@ -1,13 +1,27 @@
-// window.bears = {}
-// chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-//     window.bears[request.url] = request.count
-
-// }) 
+window.bears = {}
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+    window.bears[request.url] = request.count
+}) 
 
 // chrome.browserAction.onClicked.addListener(function(tab){
 //     chrome.tabs.create({url:'popup.html'})
 // })
 
+chrome.browserAction.onClicked.addListener(function(tab){
+    //chrome.tabs.create({url:'popup.html'})
+    
+    // const bg = chrome.extension.getBackgroundPage()
+    // Object.keys(bg.bears).forEach(function (url) {
+    //     let totalWords = bg.bears[url];
+    //     var word = totalWords.split("|");
+
+    //     // word.forEach(element => {
+    //     //     var words = Object.assign(element)
+    //     // });
+
+    //     alert(word)
+    // })
+})
 // window.bears = {}
 // chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
 //     window.bears[request.url] = request.count
@@ -15,4 +29,4 @@
 
 // chrome.browserAction.onClicked.addListener(function (tab) {
 //     chrome.tabs.create({url: 'popup.html'})
-// })
+// })   
--- Bear/popup.js
+++ Bear/popup.js
@@ -8,18 +8,78 @@ function mouseEnter() {
 function mouseLeave() {
     document.getElementById("btn").className="btn btn-dark";
 }
+document.getElementById("inp-text").onkeyup = function(){checkempty()}
+
+function checkempty(){
+    if(document.getElementById("inp-text").value === "")
+        document.getElementById("btn-add").disabled = true;
+    else{
+    document.getElementById("btn-add").disabled = false;
+    }
+}
+
+
+
+document.getElementById("btn-add").onclick= function() {addline()}
+
+function addline(){
+    // var inptext = document.getElementById("inp-text").value;
+    // document.getElementById("text").innerHTML =inptext;
+    // // console.log(inptext)
+    // document.getElementById("inp-text").value ="";
+
+    var tab = document.getElementById("tabarea")
+
+        var newchk = document.createElement('input');
+        newchk.setAttribute('type', 'checkbox');
+        newchk.setAttribute('class', 'form-check-input');
+        var newbtn= document.createElement('button');
+        newbtn.setAttribute('class', 'btn btn-dark');
+        newbtn.setAttribute('id', 'btn');
+
+        var newTd = document.createElement('td');
+        var newTd2 = document.createElement('td');
+        newTd2.setAttribute=('id','text')
+        var newTd3 = document.createElement('td');
+        var newTr = document.createElement('tr');
+        newTd.appendChild(newchk);
+        newTd.appendChild(newchk);
+        newTd.appendChild(newchk);
+        newTr.appendChild(newTd,newTd2,newTd3);
+    
+        
+    
+        tab.appendChild(newTr);  
+}
 
 
 
 
-// document.addEventListener('DOMContentLoaded', function(){
 
-//     const bg = chrome.extension.getBackgroundPage()
-//     Object.keys(bg.bears).forEach(function (url) {
-//         const div = document.createElement('div')
-//         div.textContent = `${url}: The word found is  ${bg.bears[url]}`
-//         document.body.appendChild(div)
-//     })
+
+
+
+
+document.addEventListener('DOMContentLoaded', function(){
+
+    // const bg = chrome.extension.getBackgroundPage()
+    // Object.keys(bg.bears).forEach(function (url) {
+    //     const div = document.createElement('div')
+    //     div.textContent = `${url}: The word found is  ${bg.bears[url]}`
+    //     document.body.appendChild(div)
+    // })
+    const bg = chrome.extension.getBackgroundPage()
+    Object.keys(bg.bears).forEach(function (url) {
+        let totalWords = bg.bears[url];
+        var word = totalWords.split("|");
+        
+        // word.forEach(element => {
+        //     var words = Object.assign(element)
+        // });
+
+        // alert(word)
+        console.log(url +" "+word)
+    })
     
 // const bg = chrome.extension.getBackgroundPage()
 // Object.key(bg.bears).forEach(function(url) {
@@ -41,4 +101,4 @@ function mouseLeave() {
     //     div.textContent = `${res.count} bears` 
     //     document.body.appendChild(div)
 //     // }
-// }, false)
+}, false)
