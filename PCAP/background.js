2062c72e0375f12523dd0eaaf5d8096859ce6d22
Calvin-Jefen <calvinkumambong@gmail.com>
2022-01-23 22:14:45 +0700

    Finished the requested features
    the extension can now use the words retrieved from the database of the system

--- PCAP/background.js
+++ PCAP/background.js
@@ -11,36 +11,78 @@ chrome.runtime.onInstalled.addListener(function(details){
     if (details.reason == "install") {
         try {
             var email = userEmail;
-        fetch('http://localhost/report_api/api/user', {
-                        method: 'POST',
-                        headers: {
-                            'keyapipenjualan':'p3nju4l4n',
-                            'content-type': 'application/json',
-                            authorization: 'Bearer 123abc456def'
-                        },
-                        body: JSON.stringify({
-                            user_email : email
-                        })
-                    })
-                        .then(response =>{
-                          console.log(response)
-                        })
-                            
-                        .catch(err => {
-                            console.log(err)
-                        })
-
-        getid()
+            newUser();
         } catch (error) {
             console.log(error);
         }
-        //insert the forbidden words here everytime user register
+        
+        async function newUser(){
+            const register = await registerUser(email);
+            // console.log(register);
+            if(register){
+                console.log(register)
+                setTimeout(() => getid(), 1000); 
+                setTimeout(() => sendFwords(), 2000);
+            }
+        } 
         
     } else if(details.reason == "update") {
         getid()
     }
     
 });
+
+function sendFwords(){
+    chrome.storage.local.get(['user_id'], function(result) {
+        userId = result.user_id ;
+        console.log(userId);
+        if(userId){
+            const fwords =["bear","animal","carnivore"];
+            for(i = 0; i<fwords.length;i++){
+                fetch('http://localhost/report_api/api/fwords', {
+                        method: 'POST',
+                        headers: {
+                            'keyapipenjualan':'p3nju4l4n',
+                            'content-type': 'application/json',
+                            authorization: 'Bearer 123abc456def'
+                        },
+                            body: JSON.stringify({
+                                user_id : userId,
+                                fwords : fwords[i] 
+                            })
+                        })
+                            .then(response => {
+
+                                console.log(response)
+                            })
+                            .catch(err => {
+                                console.log(err)
+                            })
+            }
+        }
+    });
+}
+function registerUser(email){
+    return fetch('http://localhost/report_api/api/user', {
+        method: 'POST',
+        headers: {
+            'keyapipenjualan':'p3nju4l4n',
+            'content-type': 'application/json',
+            authorization: 'Bearer 123abc456def'
+        },
+        body: JSON.stringify({
+            user_email : email
+        })
+    })
+        .then(response =>{
+            console.log("User Registered !")
+            return response.json()
+        })
+            
+        .catch(err => {
+            console.log(err)
+        })
+}
 
 function getid(){
     try {
@@ -64,8 +106,7 @@ function getid(){
                         var uid = data.data[0].user_id;
                         chrome.storage.local.set({user_id: uid}, function() {
                         });
-                    })
-                    
+                    })   
     } catch (error) {
         
     }
@@ -78,7 +119,8 @@ chrome.storage.local.get(['user_id'], function(result) {
     userId = result.user_id ;
     
     chrome.tabs.onUpdated.addListener( function (tabId, changeInfo, tab) {
-    
+    // sconsole.log(userId)
+    getForbiddenWords(userId)
                 const bg = chrome.extension.getBackgroundPage()
                 Object.keys(bg.PCAP).forEach(function (url) {
                     
@@ -113,24 +155,35 @@ chrome.storage.local.get(['user_id'], function(result) {
     
 });
 
-
-
-// let fwords =
-// fetch('http://localhost/report_api/api/report/fwords?user_id='+ userId, {
-//     method: 'GET',
-//     headers: {
-//         'keyapipenjualan':'p3nju4l4n',
-//         'content-type': 'application/json',
-//         authorization: 'Bearer 123abc456def'
-//     }
-// })
-//     .then(response => {
-//         console.log(userId)
-//         console.log(response)
-//     })
-//     .catch(err => {
-//         console.log(err)
-//     })
+function getForbiddenWords(uid){
+    
+    fetch('http://localhost/report_api/api/fwords?user_id='+uid, {
+        method: 'GET',
+        headers: {
+            'keyapipenjualan':'p3nju4l4n',
+            'content-type': 'application/json',
+            authorization: 'Bearer 123abc456def'
+        }
+    })
+    .then(response => {
+        return response.json()
+    }
+    )
+    .then(data => {
+        const datalen= data.data.user_data
+        var words = [];
+        for (let i = 0; i < datalen.length; i++) {
+            words [i] = data.data.user_data[i].fwords;
+        }
+        // console.log(words)
+        chrome.storage.local.set({fwords: words}, function() {
+        });
+        
+    })
+    
+    
+}
+    
 
 
 
--- PCAP/content.js
+++ PCAP/content.js
@@ -1,60 +1,63 @@
-//fetch the fwords using API here
-//assign the fetched words to a variable
-const fWords =['bear','animal','carnivore']
+chrome.storage.local.get(['fwords'], function(result) {
+const fWords = result.fwords;
 var tmp = 0;
 var words = "";
 var tmptot = 0;
 var matches = 0;
-
+//alert (fWords);
 
         try {
-           if(detect()){
-            chrome.runtime.sendMessage ({
-                url: window.location.href,
-                count : words 
+            if(detect()){
                 
-            })
-            document.body.innerHTML = `<div class="container" style=" width :100%; height:100%; background-color: rgba(255, 255, 255, .15);  
-            backdrop-filter: blur(5px);"><div>` ;
-            
-           }
-           else{
+                chrome.runtime.sendMessage ({
+                    url: window.location.href,
+                    count : words 
+                    
+                })
+            block();
+            }
+            else{
                 words = "no forbidden words found"
                 chrome.runtime.sendMessage ({
-                url: window.location.href,
-                count : words 
-            })
-           }
-          
-        } catch (error) {
-            
-        }
- 
-        function detect(){
-            try {
-                    for (let i = 0; i< fWords.length; i++) {
+                    url: window.location.href,
+                    count : words 
+                })
+            }
+        
+        
+    } catch (error) {
+        
+    }
+    
+    function detect(){
+            try { 
+                for (let i = 0; i< fWords.length; i++) {
                     matches = 0;
                     const re = new RegExp(fWords[i],'gi')
+                
                     matches = document.documentElement.innerHTML.match(re)
-        
-                    tmp = 0;
-                    tmp += matches.length
-                    tmptot  += tmp
-                    words += " "+ fWords[i]+": "+tmp+"|"
-                    
+                    if(matches){
+                        tmp = 0;
+                        tmp += matches.length
+                        tmptot  += tmp
+                        words += " "+ fWords[i]+": "+tmp+"|"
+                    }
                 }
-                return true
+                    if(words){
+                        return true
+                    }
+                else{
+                    return false
+                }
             } catch (error) {
                 return false
             } 
             
         }
-
-
-
-
-// words = {
-            //     "word" : fWords[i],
-            //     "total" : tmp
-            // };
-            // tmp = 0
+        
+        function block(){
+            document.body.innerHTML = `<div class="container" style=" width :100%; height:100%; background-color: rgba(255, 255, 255, .15);  
+            backdrop-filter: blur(5px);"><div>` ;
+        }
+        
+        })
